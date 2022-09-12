0fb41b60964bc76cb7286561fed61e43d127d2ab
Edgar <32952693+Yongyiphan@users.noreply.github.com>
2022-09-12 21:32:44 +0800

    Add project files.

---
+++ Src/SourceTemp.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void printStuff(void) {
+	printf("Hello success");
+}
---
+++ Src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "SourceTemp.h"
+
+int main(void) {
+	printStuff();
+	return 0;
+}
